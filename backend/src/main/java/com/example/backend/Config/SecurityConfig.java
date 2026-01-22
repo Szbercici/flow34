7430be716a0e1562712bcd63aea9a32afba5f22f
Q bácsi <67171470+nehezlaberzes@users.noreply.github.com>
2026-01-22 10:55:17 +0100

    Termékek is lekérhetőek bárkinek

--- backend/src/main/java/com/example/backend/Config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/Config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
+                        .requestMatchers("/api/products").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
