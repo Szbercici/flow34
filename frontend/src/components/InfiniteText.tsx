f5df033e935980294a7498ac82224940361f7ca9
Szbercici <bercelszerletics@gmail.com>
2026-02-16 09:31:32 +0100

    infinite text design

--- frontend/src/components/InfiniteText.tsx
+++ frontend/src/components/InfiniteText.tsx
@@ -5,7 +5,7 @@ const InfiniteText = () => {
     <div style={{ padding: '70px 0', background: 'var(.)', color: 'transparent', WebkitTextStroke: '2px var(--accent-color)' }}>
       <Marquee 
         speed={60}        
-        gradient={true}   
+        gradient={true}
         pauseOnHover={false} 
         autoFill={true}
       >
